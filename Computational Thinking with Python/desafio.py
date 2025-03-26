4d671f6985875295394f4e46c2c873ef47a999e9
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-25 21:33:54 -0300

    aulapython

---
+++ Computational Thinking with Python/desafio.py
@@ -0,0 +1,54 @@
+#num1 = int(input('Me dê o primeiro número: '))
+#num2 = int(input('Me dê o segundo número: '))
+#resultado = (num1 + num2)
+#print(resultado)
+import math
+
+# altura = int(input('Me dê a altura do retangulo: '))
+# base = int(input('Me dê a base do retangulo: '))
+# resultado = (num1 * num2)
+# print(resultado)
+
+# idade = int(input('Quantos anos você tem?'))
+# idadeDias = idade * 365
+# print('Sua idade em dias é ',idadeDias)
+
+
+# num1 = int(input('Me dê o primeiro número: '))
+# num2 = int(input('Me dê o segundo número: '))
+# resultadoDiv = (num1 / num2)
+# restoDiv = num1 % num2
+# print(resultadoDiv, restoDiv)
+
+# conta = float(input('Quanto foi sua conta? '))
+# dezPorc = conta * 0.1
+# print(dezPorc)
+
+# celsius = float(input('Quantos graus está em celsius?'))
+# fahrenheit = (celsius * 1.8) + 32
+# print(fahrenheit)
+
+# num = int(input('Me dê um número: '))
+# quadrado = num * num
+# raiz = math.sqrt(num)
+#
+# print(quadrado, raiz)
+
+# nome = input('Qual é seu primeiro nome? ')
+# sobrenome = input('Qual é seu sobrenome? ')
+# print(nome +' '+ sobrenome)
+
+# salário = float(input('Qual é seu salário? '))
+# Ajuste = salário * 0.15
+# resultado = Ajuste + salário
+# print(resultado)
+
+ # distancia = float(input('Quantos km você vai percorrer? '))
+# velocidade = float(input('Qual velocidade você vai? '))
+# tempo = distancia / velocidade
+# print(tempo)
+
+
+
+
+
